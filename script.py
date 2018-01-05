56ea35404880a0cd48548de44fda6da1f654662a
daeks <daeks@users.noreply.github.com>
2018-01-05 10:20:52 +0100

    v2

--- script.py
+++ script.py
@@ -18,62 +18,109 @@ total = 0
 scanned = 0
 failed = 0
 skipped = 0
+rerun = 0
 
-movies = plex.library.section(config['DEFAULT']['PLEX_SECTION'])
-for video in movies.search(sort='addedAt:desc'):
-  root_file= os.path.splitext(os.path.basename(video.locations[0]))[0]
-  log.info ('[DNET] Scanning Movie %s' % (root_file))
-  total = total + 1
+cache = '*'
+if os.path.exists(os.path.splitext(__file__)[0] + '.cache'):
+  cache = open(os.path.splitext(__file__)[0] + '.cache', 'r').read().replace('\n', '')
   
-  params = urllib.parse.urlencode({'q': root_file})
-  conn = http.client.HTTPConnection(config['DEFAULT']['API_SERVER'])
-  conn.request('GET', config['DEFAULT']['API_QUERY'] + '?' + params)
-  result = conn.getresponse()
-  data = result.read()
-  #log.info ('[DNET] API output for %s: %s' % (root_file, data))
-  
-  json_obj = json.loads(data)
-  
-  if json_obj:
-    log.info ('[DNET] Found Movie ID #%s for %s' % (json_obj['ID'], root_file))
+params = urllib.parse.urlencode({'t': cache})
+conn = http.client.HTTPConnection(config['DEFAULT']['API_SERVER'])
+url = config['DEFAULT']['API_QUERY'] + '?' + params
+log.info ('[DNET] API CALL: %s ' % url)
+conn.request('GET', url)
+result = conn.getresponse()
+data = result.read()
+#log.info ('[DNET] API output: %s (%s)' % (data, url))
+    
+json_obj = json.loads(data)
+if json_obj and len(json_obj) > 0:
+  section = plex.library.section(config['DEFAULT']['PLEX_SECTION'])
+  for obj in json_obj:
+    sync = False
+    root_file = obj['NAME'] + ' (' + obj['YEAR'] + ')'
+    log.info ('[DNET] Scanning for %s in PLEX section %s' % (root_file, config['DEFAULT']['PLEX_SECTION']))
+    total = total + 1
+    
+    if not cache == obj[config['DEFAULT']['CACHE']]:
+      cache = obj[config['DEFAULT']['CACHE']]
+    
     try:
-      if json_obj['CHECKED'] == '0':
-        status = 'New'
-        #if video.isWatched:
-        #  video.markUnwatched()
-      elif json_obj['CHECKED'] == '1':
-        status = 'Checked'
-        if not video.isWatched:
-          video.markWatched()
-      elif json_obj['CHECKED'] == '2':
-        status = 'Updated'
-        #if video.isWatched:
-        #  video.markUnWatched()
-      
-      if len(video.labels) == 1:  
-        if status == video.labels[0].tag:
-          log.info ('[DNET] Skipped Label for %s: %s' % (root_file, status))
-          skipped = skipped + 1
-          continue
-      
-      if len(video.labels) >= 1: 
-        for label in video.labels:
-          video.removeLabel(label.tag)
-          
-      if len(video.labels) == 0:
-        video.addLabel(status)
-        log.info ('[DNET] Label set to %s for %s' % (status, root_file))
-      scanned = scanned + 1
+      for video in section.search(title=obj['NAME'], sort='addedAt:desc'):
+        log.info ('[DNET] Found Movie option in PLEX : %s' % (video.originalTitle))
+        if video.originalTitle == root_file:
+          sync = True
+          log.info ('[DNET] MATCH - Found Movie in PLEX for %s (%s): %s' % (root_file, obj['ID'], video.originalTitle))      
+          try:
+            if obj['CHECKED'] == '0':
+              status = 'New'
+              #if video.isWatched:
+              #  video.markUnwatched()
+              #  log.info ('[DNET] > Switched ViewState for %s: Unwatched' % root_file)
+            elif obj['CHECKED'] == '1':
+              status = 'Checked'
+              if not video.isWatched:
+                video.markWatched()
+                log.info ('[DNET] > Switched ViewState for %s: Watched' % root_file)
+            elif obj['CHECKED'] == '2':
+              status = 'Updated'
+              #if video.isWatched:
+              #  video.markUnWatched()
+              #  log.info ('[DNET] > Switched ViewState for %s: Unwatched' % root_file)
+              
+            if obj['RATING'] != '0':
+              video.edit(**{"userRating.value": ((float(obj['RATING'])/6)*10)})
+              log.info ('[DNET] > Set userRating for %s: %s' % (root_file, ((float(obj['RATING'])/6)*10)))
+            else:
+              video.edit(**{"userRating.value": "-1"})
+              log.info ('[DNET] > Set userRating for %s: None' % root_file)
+            
+            if len(video.labels) == 1:  
+              if status == video.labels[0].tag:
+                log.info ('[DNET] > Skipped Label for %s: %s' % (root_file, status))
+                skipped = skipped + 1
+                video.refresh()
+                log.info ('[DNET] > Refreshed Metadata for %s' % root_file)
+                break
+            
+            if len(video.labels) >= 1: 
+              for label in video.labels:
+                video.removeLabel(label.tag)
+              # log.info ('[DNET] > Set Label for %s: None' % root_file)
+              video.reload()
+              # rerun = rerun + 1
+              # break
+                
+            if len(video.labels) == 0:
+              video.addLabel(status)
+              log.info ('[DNET] > Set Label for %s: %s' % (root_file, status))
+            scanned = scanned + 1
+            video.refresh()
+            log.info ('[DNET] > Refreshed Metadata for %s' % root_file)
+            break
+          except Exception as ex:
+            log.info ('[DNET] > Could not post data from API for %s: %s' % (root_file, ex))
+        else:
+          log.info ('[DNET] Skipped Movie option in PLEX : %s' % (video.originalTitle))
+    
+      if sync == False:
+        failed = failed + 1
+        params = urllib.parse.urlencode({'q': root_file, 'payload': ''})
+        conn.request("POST", config['DEFAULT']['API_HOOK'], params)
+        result = conn.getresponse()
+        if result.status == 200:
+          log.info ('[DNET] Reset PLEX SYNC for %s' % root_file)
+        else:
+          log.info ('[DNET] Could not reset PLEX SYNC for %s' % root_file)
     except Exception as ex:
-      log.info ('[DNET] Could not post data from API for %s: %s' % (root_file, ex))
-  else:
-    if len(video.labels) >= 1: 
-      for label in video.labels:
-        video.removeLabel(label.tag)
-        
-    if len(video.labels) == 0:
-      video.addLabel('None')
-      log.info ('[DNET] MOVIE NOT FOUND: %s' % (root_file))
-    failed = failed + 1
+      log.info ('[DNET] Could not communicate with PLEX for %s: %s' % (root_file, ex))
+      failed = failed + 1
+  try:
+    if rerun == 0:
+      f = open(os.path.splitext(__file__)[0] + '.cache', 'w' )
+      f.write(cache)
+      f.close()
+  except:
+    log.info ('[DNET] Unable to write cache to %s: %s' % (os.path.splitext(__file__)[0] + '.cache', ex))
   
-log.info ('[DNET] Job finished - TOTAL: %d, SCANNED: %d, SKIPPED: %d, FAILED: %d' % (total, scanned, skipped, failed))
+log.info ('[DNET] Job finished - TOTAL: %d, SCANNED: %d, SKIPPED: %d, RERUN: %d, FAILED: %d' % (total, scanned, skipped, rerun, failed))
